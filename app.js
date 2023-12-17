47a594b3d3e2e7d79100b52ceed6fd295b2a9247
sufi <sufi@sufis-MacBook-Pro.local>
2023-12-17 15:33:08 +0500

    first episode core react in a javascript file

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const parent = React.createElement("div", {id: "parent"}, 
+React.createElement("div", {id: "child"},
+[React.createElement("h1", {}, "this is h1 heading inside clild"),
+React.createElement("h2", {}, "this is h2 heading inside clild")]));
+
+
+const heading = React.createElement("h1", {id: "heading"}, "heading from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+console.log(parent);
