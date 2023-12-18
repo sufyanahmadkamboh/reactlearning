2546170cf66ee9f3e6ee4ddbc97b8c91e761322d
sufi <sufi@sufis-MacBook-Pro.local>
2023-12-19 01:26:05 +0500

    3rd episode react functional component jsx

--- app.js
+++ app.js
@@ -2,16 +2,32 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-
-const parent = React.createElement("div", {id: "parent"}, 
-React.createElement("div", {id: "child"},
-[React.createElement("h1", {}, "this is h1 heading inside clild"),
-React.createElement("h2", {}, "this is h2 heading inside clild")]));
+//react element
+const heading = React.createElement("h1",{id: "heading"},"This is our first react heading");
 
 
-const heading = React.createElement("h1", {id: "heading"}, "heading from React");
+//react element
+const headingjsx = <h1 id="heading">Hello from the react component</h1>
+
+//react functional component
+const TitleComponent = () => <h1 id="title">Hello from the react title component</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//you can only render react element like this
+root.render(headingjsx);
 
-console.log(parent);
+// you can also render a component inside a component
+
+//react functional component inside a compponent
+const HeadingComponent = () => (
+    <div id="container">
+        <TitleComponent /> 
+        <h1 id="heading">Hello from the react heading component</h1>
+    </div>
+    );
+
+
+//you can render react functional component like this
+root.render(<HeadingComponent />)
+
